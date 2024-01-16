b9344b88c35b22eee930b7d522bbbaabb9b0511e
Natalia Panfilenka <choco-cat@mail.ru>
2024-01-16 21:04:20 +0300

    getTop method

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\DB;
 
 class Result extends Model
 {
@@ -30,5 +32,20 @@ class Result extends Model
     public function members(): BelongsTo
     {
         return $this->belongsTo(Member::class, 'member_id', 'id');
+    }
+
+    /**
+     * Get members top list
+     * @param int $count
+     * @return array|Collection
+     */
+    public static function getTop(int $count): array|Collection
+    {
+        return self::with('members')
+            ->select('member_id', DB::raw('MIN(milliseconds) as milliseconds'))
+            ->groupBy('member_id')
+            ->orderBy('milliseconds')
+            ->take($count)
+            ->get();
     }
 }
