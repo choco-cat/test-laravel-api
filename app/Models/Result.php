7bb5c30af4404aa35aa3a09a00a475d048e03ec6
Natalia Panfilenka <choco-cat@mail.ru>
2024-01-17 10:35:42 +0300

    result model methods

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -29,7 +30,7 @@ class Result extends Model
      *
      * @return BelongsTo
      */
-    public function members(): BelongsTo
+    public function member(): BelongsTo
     {
         return $this->belongsTo(Member::class, 'member_id', 'id');
     }
@@ -37,15 +38,38 @@ class Result extends Model
     /**
      * Get members top list
      * @param int $count
-     * @return array|Collection
+     * @return Collection
      */
-    public static function getTop(int $count): array|Collection
+    public static function getTop(int $count): Collection
     {
-        return self::with('members')
-            ->select('member_id', DB::raw('MIN(milliseconds) as milliseconds'))
-            ->groupBy('member_id')
-            ->orderBy('milliseconds')
+        return self::queryWithBestMembersResults()
             ->take($count)
             ->get();
+    }
+
+    /**
+     * Get best member result
+     *
+     * @param string $email
+     * @return Model|null
+     */
+    public static function getUserResult(string $email): Model|null
+    {
+        return self::queryWithBestMembersResults()
+            ->whereRelation('member', 'email', $email)
+            ->first();
+    }
+
+    /**
+     * Adds members fields and grouping by members to the query
+     *
+     * @return Builder
+     */
+    private static function queryWithBestMembersResults(): Builder
+    {
+        return self::with('member')
+            ->select('member_id', DB::raw('MIN(milliseconds) as milliseconds'))
+            ->groupBy('member_id')
+            ->orderBy('milliseconds');
     }
 }
